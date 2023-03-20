dc6471fcf5f41a4f2039f03f982791275a5c5f5c
Leonardo Reis <amarantee.reis@gmail.com>
2023-03-20 08:14:27 -0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+import fastify from "fastify";
+
+export const app = fastify();
+
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+	data: {
+		name: "User Test",
+		email: "test@test.com",
+	}
+});
